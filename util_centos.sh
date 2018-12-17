29aabd5dac26fee33f1390da1021666dbdad36e3
Jimmy Xu <xjimmyshcn@gmail.com>
2018-12-17 14:08:58 +0800

    [libvirt] update certificate for kubernetes

--- util_centos.sh
+++ util_centos.sh
@@ -264,7 +264,7 @@ function ensure_dependency(){
     fi
     echo "-----------------------------"
     echo "[for libvirt] ensure vagrant plugin "
-    for p in vagrant-libvirt vagrant-mutate
+    for p in vagrant-libvirt vagrant-mutate vagrant-scp
     do
       echo "[for libvirt] ensure vagrant plugin : ${p} "
       vagrant plugin list | grep ${p} >/dev/null 2>&1
